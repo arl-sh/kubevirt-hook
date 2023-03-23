75163da314f4bd8e47e683eb9d0177a0c07122c6
Aurélien <git@garnier.dev>
2023-03-23 03:22:36 +0100

    Create base hook

--- usbdevice.go
+++ usbdevice.go
@@ -2,5 +2,67 @@
 
 package main
 
+import (
+	"context"
+	"net"
+	"os"
+	"path/filepath"
+
+	"google.golang.org/grpc"
+
+	"kubevirt.io/client-go/log"
+
+	"kubevirt.io/kubevirt/pkg/hooks"
+	hooksInfo "kubevirt.io/kubevirt/pkg/hooks/info"
+	hooksV1alpha2 "kubevirt.io/kubevirt/pkg/hooks/v1alpha2"
+)
+
+type infoServer struct{}
+
+func (s infoServer) Info(ctx context.Context, params *hooksInfo.InfoParams) (*hooksInfo.InfoResult, error) {
+	return &hooksInfo.InfoResult{
+		Name: "usbdevice",
+		Versions: []string{
+			hooksV1alpha2.Version,
+		},
+		HookPoints: []*hooksInfo.HookPoint{
+			{
+				Name:     hooksInfo.OnDefineDomainHookPointName,
+				Priority: 0,
+			},
+		},
+	}, nil
+}
+
+type v1alpha2Server struct{}
+
+func (s v1alpha2Server) OnDefineDomain(ctx context.Context, params *hooksV1alpha2.OnDefineDomainParams) (*hooksV1alpha2.OnDefineDomainResult, error) {
+	return &hooksV1alpha2.OnDefineDomainResult{
+		DomainXML: params.GetDomainXML(),
+	}, nil
+}
+
+func (s v1alpha2Server) PreCloudInitIso(_ context.Context, params *hooksV1alpha2.PreCloudInitIsoParams) (*hooksV1alpha2.PreCloudInitIsoResult, error) {
+	return &hooksV1alpha2.PreCloudInitIsoResult{
+		CloudInitData: params.GetCloudInitData(),
+	}, nil
+}
+
 func main() {
+	log.InitializeLogging("usbdevice-hook-sidecar")
+
+	socketPath := filepath.Join(hooks.HookSocketsSharedDirectory, "usbdevice.sock")
+	socket, err := net.Listen("unix", socketPath)
+	if err != nil {
+		log.Log.Reason(err).Errorf("Failed to initialized socket on path: %s", socket)
+		log.Log.Error("Check whether given directory exists and socket name is not already taken by other file")
+		panic(err)
+	}
+	defer os.Remove(socketPath)
+
+	server := grpc.NewServer([]grpc.ServerOption{}...)
+	hooksInfo.RegisterInfoServer(server, infoServer{})
+	hooksV1alpha2.RegisterCallbacksServer(server, v1alpha2Server{})
+	log.Log.Infof("Starting hook server exposing 'info' and 'v1alpha2' services on socket %s", socketPath)
+	server.Serve(socket)
 }
